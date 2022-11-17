db1e1ae7e89d161a0bd0fa5c36f6ab18a342424b
fioresat <mekhonoshinama@gmail.com>
2022-11-17 20:59:33 +0300

    diary_screen.dart

---
+++ lib/ui/screens/diary_screen/day_review_screen_store.g.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'day_review_screen_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$DayReviewScreenStore on _DayReviewScreenStore, Store {
+  late final _$reviewValueAtom =
+      Atom(name: '_DayReviewScreenStore.reviewValue', context: context);
+
+  @override
+  DayReviewModel get reviewValue {
+    _$reviewValueAtom.reportRead();
+    return super.reviewValue;
+  }
+
+  @override
+  set reviewValue(DayReviewModel value) {
+    _$reviewValueAtom.reportWrite(value, super.reviewValue, () {
+      super.reviewValue = value;
+    });
+  }
+
+  late final _$_DayReviewScreenStoreActionController =
+      ActionController(name: '_DayReviewScreenStore', context: context);
+
+  @override
+  void addDayReview(DayReviewModel dayReview) {
+    final _$actionInfo = _$_DayReviewScreenStoreActionController.startAction(
+        name: '_DayReviewScreenStore.addDayReview');
+    try {
+      return super.addDayReview(dayReview);
+    } finally {
+      _$_DayReviewScreenStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+reviewValue: ${reviewValue}
+    ''';
+  }
+}
